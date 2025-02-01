d1fba9a6730b5e8daeeb128217baa4201deb7514
Lucas Emanoel <lucaasifx@gmail.com>
2025-02-01 08:24:08 -0300

    :sparkles: Implementação da atualização da matriz por rotinas de interrupção

--- src/lib/interrupt.c
+++ src/lib/interrupt.c
@@ -1,9 +1,17 @@
 #include "button.h"
 #include "interrupt.h"
+#include "matrix.h"
 #include "pico/bootrom.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
 
 void gpio_irq_handler(uint gpio, uint32_t events) {
     // Habilita a entrada do modo BOOTSELL ao pressionar JOYSTICK_BUTTON
     if(gpio == JOYSTICK_BUTTON)
         reset_usb_boot(0,0);
+    if(gpio == BUTTON_1 && actual_number < 9)
+        actual_number++;
+    if(gpio == BUTTON_2 && actual_number)
+        actual_number--;
+    printf("Contador: %d\n", actual_number);
 }
--- src/main.c
+++ src/main.c
@@ -9,8 +9,9 @@
 
 // ************************ LIBS ***************************
 #include "lib/led.h"
-#include "button.h"
-#include "interrupt.h"
+#include "lib/button.h"
+#include "lib/interrupt.h"
+#include "lib/matrix.h"
 
 // ********************* MACROS *****************************
 
@@ -25,9 +26,15 @@ int main() {
     init_button_with_interrupt(BUTTON_2, GPIO_IRQ_EDGE_FALL, true);
     init_button_with_interrupt(JOYSTICK_BUTTON, GPIO_IRQ_EDGE_FALL, true);
   
+    PIO pio = pio0;
+    int sm = 0;
+    uint offset = pio_add_program(pio, &ws2812_program);
+
+    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
     turn_led_on(LED_RED);
     while(true) {
-
+        set_one_led(numbers[actual_number], led_r, led_g, led_b);
+        sleep_ms(100);
     }
     return 0;
 }
